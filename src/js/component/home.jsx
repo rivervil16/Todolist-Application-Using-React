cf7d8e6f02a26387376e986918404ef0e2b87bbd
rivervil16 <agustinriverovila@gmail.com>
2022-09-13 18:55:47 +0000

    queda realizada la funcionabilidad del sitio. Falta agregar Bootstrap

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,18 +1,50 @@
+import { element } from "prop-types";
 import React, { useState } from "react";
 
 const Home = () => {
-  const [list, setList] = useState([]);
   const [item, setItem] = useState("");
+  const [list, setList] = useState([]);
+  const agregarTareas = (e) => {
+    e.preventDefault();
+    let tempList = [...list];
+    tempList.push(item);
+    setList(tempList);
+    setItem("");
+  };
+  const addItem = (e) =>{
+    setItem(e.target.value);
+  };
+  const deleteItem = (indice) =>{
+    let listDeleted = list.filter(
+      (itemTarea, posicion) => posicion !== indice );
+      setList(listDeleted);
+  };
+
+
   return (
     <>
-      <div className="Home">
+      <div className="Home container">
+        <form onSubmit={agregarTareas}>
         <input
+          placeholder="agrega tu tarea aqui"
           type="text"
-          value={""}
-          onChange={(e) => setItem(e.target.value)}
+          value={item}
+          onChange={addItem}
         />
-        <button> Nueva Tarea </button>
-        <ul></ul>
+        </form>
+        <li>
+          {list.map((element, indice) => (
+            <li className="list-group-item d-flex" key={indice}>
+              <p>{element}</p>
+              <button onClick={() => 
+              deleteItem(indice)}>
+            X
+              </button>
+            </li>
+          )
+
+          )}
+        </li>
       </div>
     </>
   );
