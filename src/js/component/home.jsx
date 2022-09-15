0fbcd106651febb77dbc7a38ae5d28476a8770e2
rivervil16 <agustinriverovila@gmail.com>
2022-09-15 02:29:28 +0000

    Todo List operativo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -11,42 +11,58 @@ const Home = () => {
     setList(tempList);
     setItem("");
   };
-  const addItem = (e) =>{
+  const addItem = (e) => {
     setItem(e.target.value);
   };
-  const deleteItem = (indice) =>{
-    let listDeleted = list.filter(
-      (itemTarea, posicion) => posicion !== indice );
-      setList(listDeleted);
+  const deleteItem = (indice) => {
+    let listDeleted = list.filter((itemTarea, posicion) => posicion !== indice);
+    setList(listDeleted);
   };
 
-
   return (
-    <>
-      <div className="Home container">
-        <form onSubmit={agregarTareas}>
-        <input
-          placeholder="agrega tu tarea aqui"
-          type="text"
-          value={item}
-          onChange={addItem}
-        />
-        </form>
-        <li>
-          {list.map((element, indice) => (
-            <li className="list-group-item d-flex" key={indice}>
-              <p>{element}</p>
-              <button onClick={() => 
-              deleteItem(indice)}>
-            X
-              </button>
-            </li>
-          )
-
-          )}
-        </li>
+    <div className="container">
+      <div className="container">
+        <h1 className="text-center">Todo List</h1>
       </div>
-    </>
+      <div className="col-4 mx-auto d-flex justify-content-between p-3">
+        <div className="col-12">
+          <form onSubmit={agregarTareas}>
+            <div className="d-flex justify-content-center ">
+              <div>
+                <input
+                  className="form-control"
+                  placeholder="agrega tu tarea aqui"
+                  type="text"
+                  value={item}
+                  onChange={addItem}
+                />
+              </div>
+              <button type="submit" className="btn-sm btn-outline-secondary">
+                Add
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="col-4 mx-auto justify-content-between p-1 mb-2 bg-light text-dark border border-secondary rounded-3">
+        <div>
+          {list.map((element, indice) => (
+            <div className="d-flex justify-content-between m-1" key={indice}>
+              <p>{element}</p>
+              <button
+                className="btn btn-secondary"
+                onClick={() => deleteItem(indice)}
+              >
+                Borrar
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div>
+        <h5 className="text-center">Tareas Pendientes{" "}{list.length}</h5>
+      </div>
+    </div>
   );
 };
 export default Home;
